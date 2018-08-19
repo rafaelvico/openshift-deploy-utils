0b8ffc011d35d7bbf5f976c30a2999b6fdbd7fd8
Rafael Vico <rafael.vico@smanager.com.br>
2018-08-19 01:09:05 +0000

    correç da 1a linha do pos-uniinstall.sh

--- pos-unistall.sh
+++ pos-unistall.sh
@@ -1,4 +1,4 @@
-  #ansible nodes -m shell -a "systemctl stop docker ; yum remove docker docker-common -y ; rm -Rf /var/lib/docker/* ; yum clean all ; rm -rf /var/cache/yum"
+  ansible nodes -m shell -a "systemctl stop docker ; yum remove docker docker-common -y ; rm -Rf /var/lib/docker/* ; yum clean all ; rm -rf /var/cache/yum"
   ansible nodes -m parted -a "device=/dev/sdc number=1 state=absent"
   ansible nodes -m shell -a "wipefs -a /dev/sdc"
   ansible nodes -m shell -a "yum install docker -y"
